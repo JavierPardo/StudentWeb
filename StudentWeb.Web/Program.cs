40b6f6f31a6c69e07015b98ce3b2d60801a1638d
jpardo-visionweb-1 <152330608+jpardo-visionweb-1@users.noreply.github.com>
2024-09-28 12:23:04 -0400

    finish configuration

--- StudentWeb.Web/Program.cs
+++ StudentWeb.Web/Program.cs
@@ -18,23 +18,18 @@ builder.Services.AddTransient<IExamRepository, ExamRepository>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
-app.UseStaticFiles();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+var options = new DefaultFilesOptions();
+options.DefaultFileNames.Clear();
+options.DefaultFileNames.Add("index.html");
+app.UseDefaultFiles(options);
+app.UseStaticFiles();
 
 app.Run();
